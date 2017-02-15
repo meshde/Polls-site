3e94415e892bf7f1983e5799e61444ca7dff2a62
meshde <meshde.md@gmail.com>
2017-02-15 13:53:16 +0530

    Replace views with generic views

--- pollapp/views.py
+++ pollapp/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from .models import Question,Choice
+
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+	template_name = 'polls\\index.html'
+	context_object_name = 'lq_list'
+	
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+		
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls\\detail.html'
+	
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls\\results.html'
 
 def index(request):
 	ls = Question.objects.order_by('-pub_date')[:5]
